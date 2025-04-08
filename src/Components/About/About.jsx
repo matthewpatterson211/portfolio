6c41882fa43ee1b738d501d0c0c0b76d07b3648b
matthewpatterson211 <56987848+matthewpatterson211@users.noreply.github.com>
2025-04-08 07:16:50 -0500

    added About

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="about-sections">
+            <div className="about-left">
+                <img src={profile_img} alt="" />
+            </div>
+            <div className="about-right">
+                <div className="about-para">
+                    <p>I am a detail-oriented and motivated entry-level Web Developer with a strong passion for frontend development. While I’m early in my professional journey, I bring a solid foundation in web technologies, a commitment to writing clean and responsive code, and a genuine enthusiasm for creating engaging user experiences.</p>
+                    <p>I'm eager to contribute to dynamic teams, continue learning, and grow within the ever-evolving world of web development.</p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill"><p>HTML & CSS</p><hr style={{width: "50%"}}/></div>
+                    <div className="about-skill"><p>React</p><hr style={{width: "70%"}}/></div>
+                    <div className="about-skill"><p>JavaScript</p><hr style={{width: "60%"}}/></div>
+                    {/* <div className="about-skill"><p>Next JS</p><hr style={{width: "50%"}}/></div> */}
+                </div>
+            </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>10+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>10+</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>10+</h1>
+                <p>HAPPY CLIENTS</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
+import profile_img from '../../assets/profile_img.png'
 
 const Hero = () => {
   return (
     <div className='hero'>
-        <img src={profile_img} alt="" />
+        <img src={profile_img} alt="" width={320} height={320} />
+        <h1><span>I'm Matthew Patterson,</span> web developer based in the US.</h1>
+        <p>I am a web developer from St. Louis, MO with experience in Java, Javascript, and React. </p>
+        <div className="hero-action">
+          <div className='hero-connect'>Connect with me</div>
+          <div className="hero-resume">My resume</div>
+        </div>
     </div>
   )
 }
