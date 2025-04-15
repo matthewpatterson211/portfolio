32f7a753aaf95dc7a6307f6fabc4259a00bab269
matthewpatterson211 <56987848+matthewpatterson211@users.noreply.github.com>
2025-04-15 07:10:58 -0500

    contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
 import Services from "./Components/Services/Services";
+import MyWork from "./Components/MyWork/MyWork";
+import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Hero/>
       <About/>
       <Services/>
+      <MyWork/>
+      <Contact/>
     </div>
   )
 }
