cf687cb316e97bcbe74d21e2cecfc10f08bb8513
matthewpatterson211 <56987848+matthewpatterson211@users.noreply.github.com>
2025-04-27 07:33:49 -0500

    fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useRef } from 'react'
-import '.Navbar/Navbar.css'
+import './Navbar.css'
 import logo from '../../assets/logo.png'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
